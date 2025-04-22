bc83bf9ff577da2c42826ded0f818bc56cee0d49
GiSeok Kang <ime.rokz@gmail.com>
2025-04-22 14:42:59 +0900

    feat: id를 통한 예약 시간 삭제 api 구현
    - 예약 시간 요청 및 응답 Dto로 변경

--- src/main/java/roomescape/controller/time/ReservationTimeController.java
+++ src/main/java/roomescape/controller/time/ReservationTimeController.java
@@ -1,14 +1,17 @@
 package roomescape.controller.time;
 
-import java.time.LocalTime;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.domain.time.ReservationTime;
 import roomescape.domain.time.ReservationTimeRepository;
+import roomescape.dto.ReservationTimeRequest;
+import roomescape.dto.ReservationTimeResponse;
 
 @RestController
 public class ReservationTimeController {
@@ -20,17 +23,32 @@ public class ReservationTimeController {
     }
 
     @PostMapping("/times")
-    public ResponseEntity<ReservationTime> addTime(@RequestBody ReservationTime reservationTime) {
-        Long id = reservationTimeRepository.add(reservationTime);
+    public ResponseEntity<ReservationTimeResponse> addTime(@RequestBody ReservationTimeRequest reservationTimeRequest) {
+        Long id = reservationTimeRepository.add(new ReservationTime(null, reservationTimeRequest.startAt()));
         ReservationTime findReservationTime = reservationTimeRepository.findById(id);
 
-        return ResponseEntity.ok(findReservationTime);
+        return ResponseEntity.ok(new ReservationTimeResponse(
+                findReservationTime.getId(),
+                findReservationTime.getStartAt()
+        ));
     }
 
     @GetMapping("/times")
-    public ResponseEntity<List<ReservationTime>> getTimes() {
-        List<ReservationTime> reservations = reservationTimeRepository.findAll();
+    public ResponseEntity<List<ReservationTimeResponse>> getTimes() {
+        List<ReservationTimeResponse> response = reservationTimeRepository.findAll().stream()
+                .map(reservationTime -> new ReservationTimeResponse(
+                        reservationTime.getId(),
+                        reservationTime.getStartAt())
+                )
+                .toList();
 
-        return ResponseEntity.ok(reservations);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/times/{id}")
+    public ResponseEntity<Void> deleteTime(@PathVariable("id") Long id) {
+        reservationTimeRepository.deleteById(id);
+
+        return ResponseEntity.ok().build();
     }
 }
