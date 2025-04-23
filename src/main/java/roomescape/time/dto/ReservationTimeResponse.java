24bb7af61372702596bcdac22037a2affa5b90c2
GiSeok Kang <ime.rokz@gmail.com>
2025-04-23 20:45:52 +0900

    refactor: ReservationTime 객체 -> Dto 변환 책임 Dto로 이동

--- src/main/java/roomescape/time/dto/ReservationTimeResponse.java
+++ src/main/java/roomescape/time/dto/ReservationTimeResponse.java
@@ -1,6 +1,10 @@
 package roomescape.time.dto;
 
 import java.time.LocalTime;
+import roomescape.time.domain.ReservationTime;
 
 public record ReservationTimeResponse(Long id, LocalTime startAt) {
+    public static ReservationTimeResponse toDto(final ReservationTime findReservationTime) {
+        return new ReservationTimeResponse(findReservationTime.getId(), findReservationTime.getStartAt());
+    }
 }
