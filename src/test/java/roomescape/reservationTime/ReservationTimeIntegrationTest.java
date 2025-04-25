4021eb1fed2e31a6beb8a7ff81c41e143ab094cc
GiSeok Kang <ime.rokz@gmail.com>
2025-04-25 21:39:05 +0900

    test: 메서드 분리 및 네이밍 변경

---
+++ src/test/java/roomescape/reservationTime/ReservationTimeIntegrationTest.java
@@ -0,0 +1,42 @@
+package roomescape.reservationTime;
+
+import static org.hamcrest.CoreMatchers.is;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class ReservationTimeIntegrationTest {
+
+    @DisplayName("예약 시간을 조회하고 추가 및 삭제할 수 있다.")
+    @Test
+    void reservation_time_can_manage() {
+        Map<String, String> params = new HashMap<>();
+        params.put("startAt", "10:00");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/times")
+                .then().log().all()
+                .statusCode(200);
+
+        RestAssured.given().log().all()
+                .when().get("/times")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(1));
+
+        RestAssured.given().log().all()
+                .when().delete("/times/1")
+                .then().log().all()
+                .statusCode(200);
+    }
+}
